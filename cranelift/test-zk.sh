77d4df08458cd755a83f8423f7dcf659687ed783
Viktar Makouski <mcviktor@tut.by>
2023-09-22 17:02:18 +0300

    more flags

--- cranelift/test-zk.sh
+++ cranelift/test-zk.sh
@@ -1,55 +1,103 @@
 #!/bin/bash
 
-# ANSI escape codes for colors
-RESET="\033[0m"
-RED="\033[31m"
-GREEN="\033[32m"
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
 
-# Change to the desired directory
-cd ../../zkevm-proverjs
+# Flags and default modes
+INSTALL_MODE="preinstalled"
+ALL_FILES=false
 
-# Default behavior: operate on one provided file
-if [[ "$1" != "--all" && -n "$1" ]]; then
-    zkasm_file="../wasmtime/cranelift/$1"
-    if [[ -f "$zkasm_file" ]]; then
-        echo "Processing file: $(basename $zkasm_file)"
-        node test/zkasmtest.js "$zkasm_file"
-    else
-        echo -e "${RED}File $zkasm_file does not exist!${RESET}"
-    fi
-    exit 0
-elif [[ "$1" == "--all" ]]; then
-    # Checkout the training branch
-    git checkout training 1> /dev/null 2>/dev/null
+# Parse flags
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --all) ALL_FILES=true; shift ;;
+        --install-zkwasm-permanent) INSTALL_MODE="permanent"; shift ;;
+        --install-zkwasm) INSTALL_MODE="temporary"; shift ;;
+        --help)
+            echo "Usage: $0 [OPTIONS] [filename.zkasm]"
+            echo "Options:"
+            echo "  --all                           Test all zkasm files"
+            echo "  --install-zkwasm                Temporarily install and use zkevm-proverjs"
+            echo "  --install-zkwasm-permanent      Permanently install zkevm-proverjs"
+            echo "  --help                          Show this message"
+            exit 0
+            ;;
+        *) break ;;
+    esac
+done
 
-    # Initialize counters
-    passed_count=0
-    failed_count=0
+if [ "$ALL_FILES" = false ] && [ -z "$1" ]; then
+    echo "Please provide a filename or use the --all flag to test all files."
+    exit 1
+fi
 
-    # Iterate over all .zkasm files in the specified directory
-    for zkasm_file in ../wasmtime/cranelift/data/*.zkasm; do
-        # Print only the name of the current file
-        echo -n -e "Testing file: $(basename $zkasm_file)...\t"
 
-        # Execute the node command for each file and use grep and awk to process the output
-        output=$(node test/zkasmtest.js "$zkasm_file" | grep -Eo "cntSteps: [0-9]+")
+BASE_DIR="../wasmtime/cranelift"
 
-        # Check if output is empty or not
-        if [[ -z "$output" ]]; then
-            echo -e "${RED}ZKWASM ERROR${RESET}"
-            ((failed_count++))
+case $INSTALL_MODE in
+    "permanent")
+        echo "Cloning zkevm-proverjs into ../../ directory..."
+        git clone https://github.com/0xPolygonHermez/zkevm-proverjs/ ../../zkevm-proverjs > /dev/null 2>&1
+        cd ../../zkevm-proverjs
+        ;;
+    "temporary")
+        echo "Cloning zkevm-proverjs into /tmp directory..."
+        git clone https://github.com/0xPolygonHermez/zkevm-proverjs/ /tmp/zkevm-proverjs > /dev/null 2>&1
+        cd /tmp/zkevm-proverjs
+        BASE_DIR="../../"
+        ;;
+    "preinstalled")
+        cd ../../zkevm-proverjs
+        ;;
+esac
+
+git checkout training > /dev/null 2>&1
+
+if [ "$ALL_FILES" = true ]; then
+    PASSED=0
+    FAILED=0
+
+    for zkasm_file in $BASE_DIR/data/*.zkasm; do
+        FILENAME=$(basename $zkasm_file)
+        echo -n "Testing $FILENAME... "
+
+        OUTPUT=$(node test/zkasmtest.js $zkasm_file 2>&1)
+        if echo "$OUTPUT" | grep -q "cntSteps: [0-9]\+"; then
+            echo -e "${GREEN}OK. cntSteps = $(echo "$OUTPUT" | grep "cntSteps: [0-9]\+" | awk '{print $2}')${NC}"
+            ((PASSED++))
         else
-            echo -e "${GREEN}OK. $(echo $output | awk -F': ' '{print $1 " = " $2}')${RESET}"
-            ((passed_count++))
+            echo -e "${RED}ZKWASM ERROR${NC}"
+            ((FAILED++))
         fi
     done
 
-    # Print the final counts with coloring
-    echo -e "\n${GREEN}$passed_count files passed${RESET}"
-    echo -e "${RED}$failed_count files failed${RESET}"
-    exit $failed_count
+    echo -e "\n${GREEN}$PASSED files passed${NC}, ${RED}$FAILED files failed${NC}"
+
 else
-    echo "Usage:"
-    echo "$0 <filename.zkasm>         - Process a specific file."
-    echo "$0 --all                    - Process all .zkasm files."
+    zkasm_file="$BASE_DIR/$1"
+    if [ ! -f "$zkasm_file" ]; then
+        echo "File $zkasm_file does not exist!"
+        echo "Script executed from: ${PWD}"
+
+        B=$(dirname $0)
+        echo "Script location: ${B}"
+
+        # If we used the temporary installation mode, remove the cloned directory
+        if [ "$INSTALL_MODE" = "temporary" ]; then
+            echo "Removing temporary installation of zkevm-proverjs..."
+            rm -rf /tmp/zkevm-proverjs
+        fi
+
+        exit 1
+    fi
+
+    node test/zkasmtest.js $zkasm_file
+fi
+
+# If we used the temporary installation mode, remove the cloned directory
+if [ "$INSTALL_MODE" = "temporary" ]; then
+    echo "Removing temporary installation of zkevm-proverjs..."
+    rm -rf /tmp/zkevm-proverjs
 fi
