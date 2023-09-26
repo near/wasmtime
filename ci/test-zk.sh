59135a924b30a40122ed38e7d4e56ab7f569c998
Viktar Makouski <mcviktor@tut.by>
2023-09-26 13:57:09 +0300

    cleanup

--- ci/test-zk.sh
+++ ci/test-zk.sh
@@ -1,26 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m'
+# Should be runned from wasmtime/
+#
+# If you run in preinstalled mode, assumes that you have https://github.com/0xPolygonHermez/zkevm-rom
+# in same directory as wasmtime.
+
+set -o pipefail
+set -eox
 
 # Flags and default modes
-INSTALL_MODE="preinstalled"
+PREINSTALLED=true
 ALL_FILES=false
 
 # Parse flags
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --all) ALL_FILES=true; shift ;;
-        --install-zkwasm-permanent) INSTALL_MODE="permanent"; shift ;;
-        --install-zkwasm) INSTALL_MODE="temporary"; shift ;;
+        --install-zkwasm) PREINSTALLED=false; shift ;;
         --help)
             echo "Usage: $0 [OPTIONS] [filename.zkasm]"
             echo "Options:"
             echo "  --all                           Test all zkasm files"
             echo "  --install-zkwasm                Temporarily install and use zkevm-rom"
-            echo "  --install-zkwasm-permanent      Permanently install zkevm-rom"
             echo "  --help                          Show this message"
             exit 0
             ;;
@@ -33,57 +34,20 @@ if [ "$ALL_FILES" = false ] && [ -z "$1" ]; then
     exit 1
 fi
 
-
 BASE_DIR="../wasmtime"
 
-case $INSTALL_MODE in
-    "permanent")
-        echo "Cloning zkevm-rom into ../../ directory..."
-        git clone https://github.com/0xPolygonHermez/zkevm-rom/ ../../zkevm-rom > /dev/null 2>&1
-        cd ../../zkevm-rom
-        npm install
-        ;;
-    "temporary")
-        echo "Cloning zkevm-rom into /tmp directory..."
-        git clone https://github.com/0xPolygonHermez/zkevm-rom/ ./tmp/zkevm-rom > /dev/null 2>&1
-        cd ./tmp/zkevm-rom
-        npm install
-        BASE_DIR="../../"
-        ;;
-    "preinstalled")
-        cd ../../zkevm-rom
-        ;;
-esac
-
-
+if [ "$PREINSTALLED" = false ]; then
+    echo "Cloning zkevm-rom into /tmp directory..."
+    git clone https://github.com/0xPolygonHermez/zkevm-rom/ ./tmp/zkevm-rom > /dev/null 2>&1
+    cd ./tmp/zkevm-rom
+    npm install
+    BASE_DIR="../.."
+else
+    cd ../zkevm-rom
+fi
 
 if [ "$ALL_FILES" = true ]; then
-
     node tools/run-tests-zkasm.js $BASE_DIR/cranelift/zkasm_data/generated
-
-    exit_code=$?
-
-    # If we used the temporary installation mode, remove the cloned directory
-    if [ "$INSTALL_MODE" = "temporary" ]; then
-        echo "Removing temporary installation of zkevm-rom..."
-        rm -rf /tmp/zkevm-rom
-    fi
-
-    exit $exit_code
-
 else
-    zkasm_file="$BASE_DIR/$1"
-    # Replace all "//" with "/"
-    zkasm_file="${zkasm_file//\/\//\/}"
-
-    node tools/run-tests-zkasm.js $zkasm_file
-
-    exit_code=$?
-
-    # If we used the temporary installation mode, remove the cloned directory
-    if [ "$INSTALL_MODE" = "temporary" ]; then
-        echo "Removing temporary installation of zkevm-rom..."
-        rm -rf ./tmp
-    fi
-    exit $exit_code
+    node tools/run-tests-zkasm.js "$BASE_DIR/$1"
 fi
