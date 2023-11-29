9aaae061260140e1587fe9d04b0419eba5a74c7d
Viktar Makouski <mcviktor@tut.by>
2023-11-29 11:46:30 +0300

    zkasm: shru64

--- cranelift/codegen/src/isa/zkasm/inst/mod.rs
+++ cranelift/codegen/src/isa/zkasm/inst/mod.rs
@@ -267,6 +267,20 @@ fn zkasm_get_operands<F: Fn(VReg) -> VReg>(inst: &Inst, collector: &mut OperandC
             collector.reg_fixed_use(rs2, b0());
             collector.reg_def(rd);
         }
+        &Inst::Shru64 { rd, rs1, rs2, .. } => {
+            collector.reg_fixed_use(rs1, a0());
+            collector.reg_fixed_use(rs2, e0());
+            let mut clobbered = PRegSet::empty();
+            clobbered.add(d0().to_real_reg().unwrap().into());
+            clobbered.add(c0().to_real_reg().unwrap().into());
+            clobbered.add(b0().to_real_reg().unwrap().into());
+            clobbered.add(e0().to_real_reg().unwrap().into());
+            collector.reg_clobbers(clobbered);
+            collector.reg_fixed_def(rd, a0());
+        }
+        &Inst::Shru32 { rd, rs1, rs2, .. } => {
+            todo!();
+        }
         &Inst::Shl64 { rd, rs1, rs2, .. } => {
             collector.reg_fixed_use(rs1, a0());
             collector.reg_fixed_use(rs2, e0());
@@ -1018,6 +1032,18 @@ impl Inst {
                 let rs2_s = format_reg(rs2, allocs);
                 let rd_s = format_reg(rd.to_reg(), allocs);
                 format!("Shl64 rd = {}, rs1 = {}, rs2 = {}", rd_s, rs1_s, rs2_s)
+            }
+            &Inst::Shru64 { rd, rs1, rs2 } => {
+                let rs1_s = format_reg(rs1, allocs);
+                let rs2_s = format_reg(rs2, allocs);
+                let rd_s = format_reg(rd.to_reg(), allocs);
+                format!("Shru64 rd = {}, rs1 = {}, rs2 = {}", rd_s, rs1_s, rs2_s)
+            }
+            &Inst::Shru32 { rd, rs1, rs2 } => {
+                let rs1_s = format_reg(rs1, allocs);
+                let rs2_s = format_reg(rs2, allocs);
+                let rd_s = format_reg(rd.to_reg(), allocs);
+                format!("Shru32 rd = {}, rs1 = {}, rs2 = {}", rd_s, rs1_s, rs2_s)
             }
             &Inst::MulArith { rd, rs1, rs2 } => {
                 let rs1_s = format_reg(rs1, allocs);
