a7282c18b8c597aac5cc416a809c28450bfd57c7
Moritz <moritz.zielke@gmail.com>
2024-03-01 11:59:09 +0100

    Add `InstructionTracer.js` helper

---
+++ tests/zkasm/helpers/InstructionTracer.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+
+/**
+ * Handles the generation of traces of instructions executed at runtime.
+ */
+class InstructionTracer {
+    constructor() {
+        // Contains executed instructions in order of execution.
+        this.rawTrace = [];
+    }
+
+    /**
+     * @param {Object} ctx - The context passed by `zkevm-proverjs` to helper calls
+     * @param {Object} tag - Helper call specific information passed by `zkevm-proverjs`
+     * @param {Object} tag.params[0] - expects the identifier as first parameter of
+     *   `$${instrumentInst(...)}`
+     * 
+     * @example
+     * // To trace the execution of an `AluRRR` instruction, call
+     * // $${instrumentInst(AluRRR)}
+     */
+    eval_traceInstruction(ctx, tag) {
+        const instruction = tag.params[0].varName;
+        this.rawTrace.push(instruction);
+    }
+
+    /**
+     * Writes the raw trace to `path`.
+     * 
+     * @param {string} path 
+     */
+    writeRawTrace(path) {
+        if (typeof path !== "string") {
+            // Writing to a descriptor will append instead of replace content,
+            // which might result in invalid traces, see 
+            // https://nodejs.org/api/fs.html#using-fswritefile-with-file-descriptors
+            throw new Error("provide a file name (not descriptor) to write to");
+        }
+        fs.writeFileSync(path, this.rawTrace.join("\n"));
+    }
+}
